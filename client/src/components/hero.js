3777abb57d7c4bfcdc00e5158bad5e276d6a63d7
siphokazi <naledimuhauli@gmail.com>
2024-09-27 04:42:59 +0200

    added some styling to the hero of the website

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -1,11 +1,19 @@
 import React from 'react';
-// import header from './header';
+import Header from './header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
 
 const hero = () => {
     return (
         <>
-
+            <Header />
+            <div className='background m-5'>
+            </div>
+            <div className="content">
+                <h1 className='heading'>Vogue Vibes</h1>
+                <p>Your go-to source for style inspiration, beauty tips, and the hottest trends. Explore expert advice, product reviews, and daily looks to elevate your wardrobe and skincare routine</p>
+            </div>
 
         </>
     )
