665ce2cbe89528c7d3744b210091e7a22b64b6d6
siphokazi <naledimuhauli@gmail.com>
2024-09-28 04:43:12 +0200

    styled the post.js page

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './pages.css';
 
 function Post() {
     const { id } = useParams();
     const [post, setPost] = useState(null);
+
+    // Helper function to dynamically import images
+    const getImage = (imageName) => {
+        return `/images/${imageName}`;  // Assuming images are stored in 'public/images'
+    };
 
     useEffect(() => {
         fetch(`http://localhost:5000/api/posts/${id}`)
@@ -17,11 +23,23 @@ function Post() {
     }
 
     return (
-        <div>
-            <h1>{post.title}</h1>
-            <p>{post.content}</p>
+        <div className="container containers">
+            <div className="post">
+                <h1 className='post-h1'>{post.title}</h1>
+
+                <p className='post-p'>{post.content}</p>
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src={getImage(post.image)} alt={post.title} className="img-fluid post-img" />
+                    </div>
+                    <div className="col-md-6">
+                        <p className='post-description'>{post.description}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
+
 }
 
 export default Post;
